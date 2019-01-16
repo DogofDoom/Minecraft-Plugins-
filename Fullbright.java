fc9e5ec51b2af2a5943385363aed64593b439f3b
DogofDoom <dogofdoom3@gmail.com>
2019-01-16 14:51:17 +0100

    Fullbright.java

---
+++ Fullbright.java
@@ -0,0 +1,11 @@
+public class Fullbright
+
+	public static void class 
+   var toggle = none{
+   var untoggle = none
+		{set.minecraftObject.Brightness class int new function()
+		set.instanceof Fullbright = max / 100000;
+	}
+	global.chat("[CLIENT] Fullbright toggled")
+	if untoggle = true.getInformation / normalSet
+}
